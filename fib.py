7eea7b5e68b5990c40af53b145b09c9f70439beb
goode8 <drgood@umich.edu>
2021-11-26 19:38:25 -0800

    adding comments

--- fib.py
+++ fib.py
@@ -3,9 +3,10 @@
 fibb=[0,1]
 fibb[0] = 0
 fibb[1] = 1
-maxvar=20 #num of slots from 0..
+maxvar=20 #num of slots from 0 to end
 #def fibin(maxvar)
 for countval in range(2,maxvar):
+	#must use insert to add values to the list
 	fibb.insert((countval),fibb[countval-1]+fibb[countval-2])
 	print(fibb)
 		
