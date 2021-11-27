c68bc2fc66113b597eca1a68de550dcd8f57e3cf
goode8 <drgood@umich.edu>
2021-11-26 19:31:39 -0800

    removing master branch

---
+++ fib.py
@@ -0,0 +1,13 @@
+#let's do a Fibonacci series...
+#start with 0 and 1 
+fibb=[0,1]
+fibb[0] = 0
+fibb[1] = 1
+maxvar=20 #num of slots from 0..
+#def fibin(maxvar)
+for countval in range(2,maxvar):
+	fibb.insert((countval),fibb[countval-1]+fibb[countval-2])
+	print(fibb)
+		
+
+	
