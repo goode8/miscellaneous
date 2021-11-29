0b6a20d07efed8903b2997ae5252354c01a9fcc4
goode8 <64971371+goode8@users.noreply.github.com>
2021-11-28 21:01:48 -0800

    Update fib.py
    add to-do note for "ask for input"

--- fib.py
+++ fib.py
@@ -1,13 +1,11 @@
-<<<<<<< HEAD
+
 #let's do a Fibonacci series...
-=======
-#hello, let's do a Fibonacci series...
->>>>>>> 52af6d368f51bd4c7bbeb2febf060cf828c02bbf
 #start with 0 and 1 
 fibb=[0,1]
 fibb[0] = 0
 fibb[1] = 1
 maxvar=20 #num of slots from 0 to end
+##TO DO: ADD INPUT of maxvar!!###########
 #def fibin(maxvar)
 for countval in range(2,maxvar):
 	#must use insert to add values to the list
